7de8bbf1bc4359983360aef9e72fac791f7bb500
Ilya Raskin <iraskin@mail.ru>
2019-04-09 01:58:35 +0300

    Task2

---
+++ Exceptions/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "exceptions.h"
+#include "testing.cpp"
+
+int main() {
+    cout << "-----------" << endl;
+    Test1();
+    cout << "-----------" << endl;
+    Test2();
+    cout << "-----------" << endl;
+    Test3();
+    cout << "-----------" << endl;
+    Test4();
+    cout << "-----------" << endl;
+    return 0;
+}
